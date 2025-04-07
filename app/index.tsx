3375d97780ea4fcccf0ed9964c409d33cbeccd9b
Ugne <vaitkunaite.u@gmail.com>
2025-04-08 00:14:51 +0300

    Projekto Kūrimo Diena 7 - LangQuest: Kalbų mokymosi žaidimas (Expo)

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,73 @@
-import { Stack, Link } from 'expo-router';
-
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import React, { useEffect, useState } from 'react';
+import { Alert, StatusBar } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import questions from '~/assets/data/AllQuestionsData';
+import MultipleChoiceQuestion from './MultipleChoiceQuestion';
+import EndedQuestion from '~/components/EndedQuestion';
+import { QuizQuestion } from '~/types';
+import HeaderComponent from '~/components/HeaderComponent';
 
 export default function Home() {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState<QuizQuestion>(
+    questions[currentQuestionIndex]
+  );
+  const [lives, setLives] = useState(5);
+
+  useEffect(() => {
+    if (currentQuestionIndex >= questions.length) {
+      Alert.alert('Jūs laimėjote!');
+      setCurrentQuestionIndex(0);
+    } else {
+      setCurrentQuestion(questions[currentQuestionIndex]);
+    }
+  }, [currentQuestionIndex]);
+
+  const onCorrectAnswer = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+
+  const onWrongAnswer = () => {
+    if (lives <= 1) {
+      Alert.alert('Jus pralaimejote!', 'Bandykite dar karta!', [
+        { text: 'Bandyti dar karta', onPress: restart },
+      ]);
+      setLives(0);
+    } else {
+      Alert.alert('Neteisingas atsakymas, bandykite dar kartą!');
+      setLives(lives - 1);
+    }
+  };
+
+  const restart = () => {
+    setLives(5);
+    setCurrentQuestionIndex(0);
+  };
+
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <SafeAreaView className="flex flex-1 p-3">
+      <StatusBar animated barStyle={'default'} />
+
+      {/* Header */}
+      <HeaderComponent progress={currentQuestionIndex / questions.length} lives={lives} />
+
+      {currentQuestion.type === 'MULTIPLE_CHOICE' && (
+        <MultipleChoiceQuestion
+          question={{
+            question: currentQuestion.text,
+            options: currentQuestion.options || [],
+          }}
+          onCorrectAnswer={onCorrectAnswer}
+          onWrongAnswer={onWrongAnswer}
+        />
+      )}
+      {currentQuestion.type === 'OPEN_ENDED' && (
+        <EndedQuestion
+          question={currentQuestion}
+          onCorrectAnswer={onCorrectAnswer}
+          onWrongAnswer={onWrongAnswer}
+        />
+      )}
+    </SafeAreaView>
   );
 }
