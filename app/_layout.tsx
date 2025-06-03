1087b42e997fea5356f4df48f5467fbb87706521
Your Name <you@example.com>
2025-06-03 18:29:54 +0300

    atnaujinimas lay

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ export default function Layout() {
   return (
     <Stack>
       <Stack.Screen name='index' options ={{ headerShown: false}} />
+      <Stack.Screen name='mainscreen' options ={{ headerShown: false}} />
     </Stack>
   );
 }
